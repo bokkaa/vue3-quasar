5b1e76a425b2be5d584248660cbc95db449a6bc6
bokkaa <bokyh0327@gmail.com>
2024-02-13 19:25:05 +0900

    --

---
+++ src/components/apps/post/PostList.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-list bordered separator>
+    <PostItem v-for="item in items" :key="item.id" v-bind="item" />
+  </q-list>
+</template>
+
+<script setup>
+import PostItem from './PostItem.vue';
+
+// 받아온 props 데이터 정의
+defineProps({
+  items: Array,
+  // 기본값 빈 배열
+  default: () => [],
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,20 +2,35 @@
   <q-page padding>
     <div class="text-h4">커뮤니티 목록</div>
     <section class="q-gutter-y-sm q-mt-lg">
-      <q-card v-for="id in 100" :key="id" @click="goPostDetails(id)">
-        <q-card-section>{{ id }}번 게시글</q-card-section>
-      </q-card>
+      <!-- posts데이터를 items라는 이름으로 props (자식 컴포넌트로 방출) -->
+      <PostList :items="posts" />
     </section>
   </q-page>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
+import PostList from 'src/components/apps/post/PostList.vue';
 
 const router = useRouter();
 const goPostDetails = id => {
-  router.push(`/posts/${id}`);
+  // router.push(`/posts/${id}`);
 };
+
+// 더미데이터
+const posts = Array.from(Array(20), (_, index) => ({
+  id: index,
+  title: 'vue3 FireBase 강의' + index,
+  contents:
+    ' Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam eaqueaspernatur quos explicabo expedita recusandae. Harum, exercitationemimpedit reiciendis, corrupti sequi quisquam magnam, non alias modi in abnatus?',
+  readCount: 1,
+  commentCount: 2,
+  likeCount: 3,
+  bookmarkCount: 4,
+  tags: ['html', 'css', 'javascript'],
+  uid: 'uid',
+  category: '카테고리' + index,
+}));
 </script>
 
 <style lang="scss" scoped>
